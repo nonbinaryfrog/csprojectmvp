c1c0eb828c6161c28997230dfa3cf48e92eeafc4
Rhys <52160097+nonbinaryfrog@users.noreply.github.com>
2021-02-03 12:30:56 -0500

    "Created book card component"

---
+++ client/public/src/components/views/BookCardView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>Book Card</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BookCardVue",
+  props: {
+    // Add props here
+  },
+};
+</script>
+
+<style></style>
