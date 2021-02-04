423d1b82c162ef70d668624a85f5523f70f035cc
Rhys <52160097+nonbinaryfrog@users.noreply.github.com>
2021-02-04 13:44:00 -0500

    "Added on click event"

--- src/components/views/YAGenres.vue
+++ src/components/views/YAGenres.vue
@@ -2,7 +2,7 @@
   <div class="book-options">
     <h2>Young Adult: Choose a subgenre!</h2>
     <button id="get-all-ya"> Get me everything! </button>
-    <button class="subgenre" id="fantasy">Fantasy</button>
+    <button @click="getBook" class="subgenre" id="fantasy">Fantasy</button>
     <button class="subgenre" id="rom-com">Rom-com</button>
     <button class="subgenre" id="horror">Horror</button>
   </div>
@@ -12,7 +12,13 @@
 export default {
   name: "YAGenres",
   props: ["books"],
+  methods: {
+    getBook() {
+      console.log("Button pressed!")
+    }
+  }
 };
+
 </script>
 
 <style>
