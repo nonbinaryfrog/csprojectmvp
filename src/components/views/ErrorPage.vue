3b92169b3e77ad7221df52c1c276485bb8df289c
Rhys <52160097+nonbinaryfrog@users.noreply.github.com>
2021-02-12 15:56:24 -0500

    "Implemented error handling"

---
+++ src/components/views/ErrorPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="error">
+    <h2>Sorry! We couldn't find any books here.</h2>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "ErrorPage",
+};
+</script>
+
+<style>
+
+</style>
--- src/router/router.js
+++ src/router/router.js
@@ -7,41 +7,47 @@ import MGGenres from "../components/views/MGGenres.vue";
 import YAGenres from "../components/views/YAGenres.vue";
 import AdultGenres from "../components/views/AdultGenres.vue";
 import BookData from "../BookData";
+import ErrorPage from "../components/views/ErrorPage.vue";
 import { createWebHistory, createRouter } from "vue-router";
 
 // filter data based on genres first
-let pbBooks = BookData.filter(book => book.genre === 'PB');
-let mgBooks = BookData.filter(book => book.genre === 'MG');
-let yaBooks = BookData.filter(book => book.genre === 'YA');
-let adultBooks = BookData.filter(book => book.genre === 'A');
+let pbBooks = BookData.filter((book) => book.genre === "PB");
+let mgBooks = BookData.filter((book) => book.genre === "MG");
+let yaBooks = BookData.filter((book) => book.genre === "YA");
+let adultBooks = BookData.filter((book) => book.genre === "A");
 
 const routes = [
   // nested routes for genres in home
-  { path: "/", component: Home,
-  children: [
-    {
-      path: '/PB',
-      component: PBGenres,
-      props: { books: pbBooks },
-    },
-    {
-      path: '/MG',
-      component: MGGenres,
-      props: { books: mgBooks },
-    },
-    {
-      path: '/YA',
-      component: YAGenres,
-      props: { books: yaBooks },
-    },
-    {
-      path: '/Adult',
-      component: AdultGenres,
-      props: { books: adultBooks },
-    }
-  ]
-
-},
+  {
+    path: "/",
+    component: Home,
+    children: [
+      {
+        path: "/PB",
+        component: PBGenres,
+        props: { books: pbBooks },
+      },
+      {
+        path: "/MG",
+        component: MGGenres,
+        props: { books: mgBooks },
+      },
+      {
+        path: "/YA",
+        component: YAGenres,
+        props: { books: yaBooks },
+      },
+      {
+        path: "/Adult",
+        component: AdultGenres,
+        props: { books: adultBooks },
+      },
+      {
+        path: "/Error",
+        component: ErrorPage
+      },
+    ],
+  },
   { path: "/Search", component: Search },
   { path: "/About", component: About },
   { path: "/Submit", component: SubmitForm },
@@ -49,4 +55,4 @@ const routes = [
 
 const router = createRouter({ history: createWebHistory(), routes });
 
-export default router
+export default router;
