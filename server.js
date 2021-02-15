b82d41c6d210cd7071091cb43286c39387f68666
Rhys <52160097+nonbinaryfrog@users.noreply.github.com>
2021-02-15 15:22:19 -0500

    "Setting up server"

--- server.js
+++ server.js
@@ -1,19 +1,31 @@
 const express = require('express');
 const cors = require('cors');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv').config();
+
+if (dotenv.error) {
+  throw dotenv.error
+}
+
+const db = require('db')
+db.connect ({
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS
+})
 
 // will not use api folder yet
-// global.Task = require('./api/models/model');
-// const routes = require('./api/routes/routes');
+global.Task = require('./api/models/model');
+const routes = require('./api/routes/routes');
 
 // To use for connecting to the MongoDB database
-// mongoose.Promise = global.Promise;
-// mongoose.set('useFindAndModify', false);
-// mongoose.connect(
-//   'mongodb://localhost/CSProjectDB',
-//   { useNewUrlParser: true }
-// );
+mongoose.Promise = global.Promise;
+mongoose.set('useFindAndModify', false);
+mongoose.connect(
+  'mongodb://localhost/3000',
+  { useNewUrlParser: true }
+);
 
 const port = process.env.PORT || 3000;
 const app = express();
