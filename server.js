d51ae43a415465092056cca0b9d2ba5a528984c5
Rhys <52160097+nonbinaryfrog@users.noreply.github.com>
2021-02-16 12:38:24 -0500

    "Debugged server"

--- server.js
+++ server.js
@@ -8,28 +8,20 @@ if (dotenv.error) {
   throw dotenv.error
 }
 
-const MongoClient = require('mongodb').MongoClient
-MongoClient.connect ({
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS
-})
-
-const URI = 'mongodb+srv://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@cluster0.ryk29.mongodb.net/CSProjectDB?retryWrites=true&w=majority';
 // will not use api folder yet
 // global.Task = require('./api/models/model');
 // const routes = require('./api/routes/routes');
 
 // To use for connecting to the MongoDB database
-MongoClient.Promise = global.Promise;
+mongoose.Promise = global.Promise;
 mongoose.set('useFindAndModify', false);
-MongoClient.connect( URI,
+mongoose.connect( process.env.MONGO_URI,
   { useNewUrlParser: true,
   useUnifiedTopology: true })
   .then(client => {
     console.log('Connected to Database')
   })
-  .catch(error => console.error(error))
+  .catch(error => {console.error(error)})
 
 const port = process.env.PORT || 3000;
 const app = express();
