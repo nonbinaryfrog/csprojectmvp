537649d47d0c8e156c17f5be7081f4d575fce749
Rhys <52160097+nonbinaryfrog@users.noreply.github.com>
2021-06-14 18:47:20 -0400

    "Created server error page"

---
+++ src/components/views/ServerError.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="error">
+    <h2>Sorry! There seems to be a server error.</h2>
+    <p>Don't worry! We'll back soon.</p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "ServerError",
+};
+</script>
+
+<style>
+.error {
+  text-align: center;
+}
+
+</style>
