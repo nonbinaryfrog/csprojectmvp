66b0cefab62a09ef3c559177be8573130fb66e12
Rhys <52160097+nonbinaryfrog@users.noreply.github.com>
2021-06-15 17:23:35 -0400

    "Updating search form"

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,86 +1,75 @@
 <template>
-    <div id="search">
-        <form>
-            <div class="text-field">
-                <label for="search">Search by Author or Title:</label>
-                <input
-                    type="text"
-                    v-model="search"
-                    placeholder="Begin typing..." />
-            </div>
-            <!-- <button>Submit</button> -->
-        </form>
+  <div id="search">
+    <form action="#" @submit.prevent="onSubmit">
+      <div class="text-field">
+        <label for="search">Search by Author or Title:</label>
+        <input type="text" v-model="search" placeholder="Begin typing..." />
+      </div>
+      <button>Submit</button>
+    </form>
 
-    <BookCardView :books="filterList" />
-    </div>
+    <BookCardView :books="filteredBooks" />
+  </div>
 </template>
 
 <script>
 import BookCardView from "./views/BookCardView";
-// import axios from 'axios'
+import axios from "axios";
 
 export default {
-    name: "Search",
-    props: ["books"],
-    components: {
-        BookCardView,
-    },
+  name: "Search",
+  props: ["books"],
+  components: {
+    BookCardView,
+  },
 
-    data() {
-        return {
-        // initialize filteredBooks as the full set of books.
-        filteredBooks: this.books,
-        search: "",
-        };
-    },
+  data() {
+    return {
+      // initialize filteredBooks as the full set of books.
+      filteredBooks: [],
+      search: "",
+    };
+  },
 
-    // methods: {
-    //   async getBook() {
-    //   try {
-    //     let book = await axios.get('http://localhost:3000/books/YA/Fantasy', {
-    //       method: 'GET',
-    //       headers: {'Content-Type': 'application/json'},
-    //     })
-    //     if (book) {
-    //       this.filteredBooks = book.data;
-    //     } else {
-    //       this.$router.push('/Error');
-    //     }
-    //   } catch (err) {
-    //     this.$router.push("/ServerError")
-    //     console.log(`Network error: ${err.message}`)
-    //   }
-    // }
-    // },
-
-    computed: {
-        filterList() {
-            if (this.search !== "") {
-                return this.filteredBooks.filter((book) => {
-                    return (book.author[0].toLowerCase().match(this.search.toLowerCase()) 
-                    || book.title.toLowerCase().match(this.search.toLowerCase())
-                    )
-                });
-            }
-
-            return this.filteredBooks
+  methods: {
+    async onSubmit() {
+      if (this.search !== "") {
+        try {
+          let bookOrAuthor = await axios.get("http://localhost:3000/Search", {
+            method: "GET",
+            headers: { "Content-Type": "application/json" },
+            params: {
+              author: this.search,
+              title: this.search,
+            },
+          });
+          if (bookOrAuthor) {
+            this.filteredBooks = bookOrAuthor.data;
+          } else {
+            this.$router.push("/Error");
+          }
+        } catch (err) {
+          this.$router.push("/ServerError");
+          console.log(`Network error: ${err.message}`);
         }
+      }
     },
-}
+  },
+};
 </script>
 
 <style>
 #search {
-    margin: 0 auto;
-    width: 50%;
-    text-align: center;
-    /* margin-top: 25px; */
+  margin: 0 auto;
+  width: 50%;
+  text-align: center;
+  /* margin-top: 25px; */
 }
 
 .btn {
   display: grid;
   margin: 20px;
-  font-family: 'Coming Soon', cursive;
+  font-family: "Coming Soon", cursive;
 }
 
 #everything-btn {
