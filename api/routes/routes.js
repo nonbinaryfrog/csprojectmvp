abdde79b9ea2cae1475034e586c4a55478523966
Rhys <52160097+nonbinaryfrog@users.noreply.github.com>
2021-06-15 17:23:15 -0400

    "Added search route"

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,19 +1,21 @@
 // routes for books
 
-const bookBuilder = require('../controllers/controller');
-let router = require('express').Router();
+const bookBuilder = require("../controllers/controller");
+let router = require("express").Router();
 
+// GET all books
+router.get("/books", bookBuilder.list_all_books);
 
-  // GET all books
-  router.get('/books', bookBuilder.list_all_books);
+// GET books based on the genre
+router.get("/books/:genre", bookBuilder.get_some_books);
 
-  // GET books based on the genre
-  router.get('/books/:genre', bookBuilder.get_some_books)
+// GET books based on multiple queries
+router.get("/books/:genre/:subgenre", bookBuilder.get_these_books);
 
-  // GET books based on multiple queries
-  router.get('/books/:genre/:subgenre', bookBuilder.get_these_books)
+// GET book based on title or author
+router.get("/Search", bookBuilder.list_author_books);
 
-  // POST a new book entry
-  router.post('/Submit', bookBuilder.create_a_book);
+// POST a new book entry
+router.post("/Submit", bookBuilder.create_a_book);
 
 module.exports = router;
